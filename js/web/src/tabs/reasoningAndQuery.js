2796d11ec2eaa95870698b4ecf254ed73503fb26
maartenvandenbrande <maarten.vdbrande@gmail.com>
2022-11-12 17:18:39 +0100

    refactoring and implementation of share button and yasgui

---
+++ js/web/src/tabs/reasoningAndQuery.js
@@ -0,0 +1,84 @@
+import {RoxiReasoner, JSBinding} from "roxi";
+import Yasqe from "@triply/yasqe";
+
+const aboxInitialContents = "<http://example2.com/a> <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://www.test.be/test#SubClass> .";
+const tboxInitialContents = "@prefix test: <http://www.test.be/test#>.\n @prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>.\n {?s rdf:type test:SubClass. }=>{?s rdf:type test:SuperType.}";
+const queryInitialContents = "PREFIX test: <http://www.test.be/test#>. \nPREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>.\nSELECT * WHERE {\n\t?s ?p ?o.\n}";
+
+
+const aboxElement = document.getElementById('aboxContentQ');
+const tboxElement = document.getElementById('rulesContentQ');
+//const queryElement = document.getElementById('queryQ');
+const reasoningShareButton = document.getElementById("shareReasoningQ");
+
+const yasqe = new Yasqe(
+    document.getElementById('queryQ')
+);
+
+aboxElement.value = aboxInitialContents;
+tboxElement.value = tboxInitialContents;
+yasqe.setValue(queryInitialContents);
+
+
+const startReasoning = () => {
+    const reasoner = RoxiReasoner.new();
+
+    let startTime = performance.now();
+
+    reasoner.add_abox(aboxElement.value);
+    reasoner.add_rules(tboxElement.value);
+    //yasqe.getValue();
+    reasoner.materialize();
+
+    let endTime = performance.now();
+    let difftime = endTime-startTime ;
+
+    /*
+    var response={head:{vars:headVars},results:{bindings:results}};
+    console.log(response);
+    yasre.setResponse(response);
+     */
+
+    document.getElementById('resultsQ').value = reasoner.get_abox_dump();
+    document.getElementById('timeResultsQ').innerHTML = difftime + " ms";
+};
+
+const shareReasoning = () =>{
+    let host = window.location.href.split('?')[0];
+    let encodedAbox = encodeURIComponent(aboxElement.value);
+    let encodedRules = encodeURIComponent(tboxElement.value);
+    let encodedQuery = encodeURIComponent(yasqe.getValue());
+
+    let result = host +'?view=rq&abox='+encodedAbox+'&rules='+encodedRules+'&query='+encodedQuery;
+
+    navigator.clipboard
+        .writeText(result)
+        .then(
+            success => {
+                reasoningShareButton.style.backgroundColor = "#43b343";
+                document.getElementById("shareReasoningTextQ").style.opacity = "1";
+                setTimeout(()=>{
+                    reasoningShareButton.style.backgroundColor = "";
+                    document.getElementById("shareReasoningTextQ").style.opacity = "0";
+                }, 1000);
+            },
+            err => {
+                reasoningShareButton.style.backgroundColor = "#e83131";
+                setTimeout(()=>{
+                    reasoningShareButton.style.backgroundColor = "";
+                }, 1000);
+                // activate share text area
+                document.getElementById('shareIDQ').style.display = "block";
+                // display the url
+                document.getElementById('shareBoxQ').value = result;
+            }
+        );
+}
+
+document.getElementById("startReasoningQ").addEventListener("click", event => {
+    startReasoning();
+});
+
+reasoningShareButton.addEventListener("click", event => {
+    shareReasoning();
+});
---
+++ js/web/src/tabs/rsp.js
@@ -0,0 +1,128 @@
+import {JSRSPEngine, JSBinding} from "roxi";
+import Yasqe from "@triply/yasqe";
+import Yasr from "@triply/yasr";
+
+const rules = "@prefix test: <http://test/>.\n@prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>.\n{?x test:isIn ?y. ?y test:isIn ?z. }=>{?x test:isIn ?z.}";
+const query = "Select * WHERE{ ?x <http://test/isIn> ?y}";
+
+const tboxElement = document.getElementById('rulesContentRSP');
+const windowWidthElement = document.getElementById('windowWidth');
+const windowSlideElement = document.getElementById('windowSlide');
+const eventIDElement = document.getElementById('eventID');
+const timestampElement = document.getElementById('timestamp');
+const reasoningShareButton = document.getElementById("shareReasoningRSP");
+const rspButton = document.getElementById("startRSP");
+
+const yasqe = new Yasqe(
+    document.getElementById('queryRSP')
+);
+
+const yasr = new Yasr(
+    document.getElementById('resultsRSP')
+);
+
+yasr.setResponse({head:{vars:[]},results:{bindings:[]}});
+
+tboxElement.value = rules;
+yasqe.setValue(query);
+let currentTs = 0;
+let rspEngine = null;
+let results = [];
+
+const bindingRegex = new RegExp(/Binding{"(.+)": "(.+)"}/);
+const urlRegex = new RegExp(/<?https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&\/\/=]*)>?/);
+
+// callback function
+function callback(val) {
+    let RegExpExecArray = [];
+    let headVars = [];
+    let temp = {};
+    headVars.push("timestamp (not a binding)");
+    temp["timestamp (not a binding)"] = {type:"literal",value: currentTs.toString()};
+    for (const binding of val) {
+        RegExpExecArray = bindingRegex.exec(binding.toString());
+        headVars.push(RegExpExecArray[1]);
+        temp[RegExpExecArray[1]] = {type:urlRegex.test(RegExpExecArray[2])? "uri" : "literal",value: RegExpExecArray[2]};
+        //console.log(binding.toString())
+    }
+    results.push(temp)
+    var response={head:{vars:headVars},results:{bindings:results}};
+    yasr.setResponse(response);
+    console.log(response);
+    //document.getElementById('resultsRSP').value = val +"@"+currentTs+"\n" + document.getElementById('resultsRSP').value;
+}
+
+const startRSP = () => {
+    if(rspEngine == null){
+        console.log("starting");
+        let tbox_new = tboxElement.value;
+        tboxElement.setAttribute('disabled', '');
+
+        let abox = "";
+        let query = yasqe.getValue();
+        //yasqe.setAttribute('disabled', '');
+
+        let width = windowWidthElement.value;
+        windowWidthElement.setAttribute('disabled', '');
+        let slide = windowSlideElement.value;
+        windowSlideElement.setAttribute('disabled', '');
+        rspEngine = JSRSPEngine.new(width,slide,tbox_new,abox,query,callback);
+    }
+    currentTs+=1;
+    let event = eventIDElement.value;
+    rspEngine.add(event, currentTs);
+    timestampElement.value = currentTs;
+    eventIDElement.value = "<http://test/"+currentTs+"> <http://test/isIn> <http://test/"+(currentTs+1)+">.";
+
+    console.log("stopped");
+}
+
+const shareReasoning = () =>{
+    let host = window.location.href.split('?')[0];
+    let encodedRules = encodeURIComponent(tboxElement.value);
+    let encodedQuery = encodeURIComponent(yasqe.getValue());
+    let encodedWindowWidth = encodeURIComponent(windowWidthElement.value);
+    let encodedWindowSlide = encodeURIComponent(windowSlideElement.value);
+    let encodedEventID = encodeURIComponent(eventIDElement.value);
+    let encodedTimestamp = encodeURIComponent(timestampElement.value);
+
+    let result = host +
+        '?view=rsp' +
+        '&rules=' + encodedRules +
+        '&query=' + encodedQuery +
+        '&windowWidth=' + encodedWindowWidth +
+        '&windowSlide=' + encodedWindowSlide +
+        '&eventID=' + encodedEventID +
+        '&timestamp=' + encodedTimestamp;
+
+    navigator.clipboard
+        .writeText(result)
+        .then(
+            success => {
+                reasoningShareButton.style.backgroundColor = "#43b343";
+                document.getElementById("shareReasoningTextRSP").style.opacity = "1";
+                setTimeout(()=>{
+                    reasoningShareButton.style.backgroundColor = "";
+                    document.getElementById("shareReasoningTextRSP").style.opacity = "0";
+                }, 1000);
+            },
+            err => {
+                reasoningShareButton.style.backgroundColor = "#e83131";
+                setTimeout(()=>{
+                    reasoningShareButton.style.backgroundColor = "";
+                }, 1000);
+                // activate share text area
+                document.getElementById('shareIDR').style.display = "block";
+                // display the url
+                document.getElementById('shareBoxR').value = result;
+            }
+        );
+}
+
+rspButton.addEventListener("click", event => {
+    startRSP();
+});
+
+reasoningShareButton.addEventListener("click", event => {
+    shareReasoning();
+});
